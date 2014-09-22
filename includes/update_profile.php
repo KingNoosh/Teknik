9a3cec9c20a4d2be71c01ee1534cbecc6f4f265d
Uncled1023 <admin@teknik.io>
2014-09-22 01:36:30 -0700

    I'm getting tired

--- includes/update_profile.php
+++ includes/update_profile.php
@@ -134,7 +134,7 @@ if(isset($_POST))
           $Git = new Git();
           $Git->windows_mode();
           $repo = $Git->open($CONF['git_repo_path'][0].'gitolite-admin\\');
-          $repo->setenv("HOME", "/home/git");
+          $repo->setenv("HOME", "/home/Administrator");
           $repo->pull('origin', 'master');
           
           if (is_dir($CONF['git_repo_path'][0].'gitolite-admin\\keydir\\u\\'.$user->username))
