55a3042c8a14093d0d824d60a1c72c9402a47bb1
Uncled1023 <admin@teknik.io>
2014-09-23 19:26:57 -0700

    Added full pull down

--- includes/update_profile.php
+++ includes/update_profile.php
@@ -135,7 +135,8 @@ if(isset($_POST))
           $Git->windows_mode();
           $repo = $Git->open($CONF['git_repo_path'][0].'gitolite-admin\\');
           $repo->setenv('HOME', '/home/iusr');
-          $repo->pull('origin', 'master');
+          $repo->fetch();
+          $repo->run('reset --hard origin/master');
           
           if (is_dir($CONF['git_repo_path'][0].'gitolite-admin\\keydir\\u\\'.$user->username))
           {
