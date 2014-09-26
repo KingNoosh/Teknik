5d2e4d5581abec38007e5f55177fe7fb4fbaa718
Uncled1023 <admin@teknik.io>
2014-09-25 19:18:24 -0700

    Moved CSS to footer

--- templates/default/footer.php
+++ templates/default/footer.php
@@ -4,6 +4,7 @@
           <p class="text-muted">&copy; Teknik 2013-2014 | <a href="<?php echo get_page_url("privacy", $CONF); ?>">Privacy</a> | <a href="<?php echo get_page_url("transparency", $CONF); ?>">Transparency</a> | <a href="<?php echo get_page_url("stats", $CONF); ?>">Stats</a></p>
       </div>
     </footer>
+    <link href="<?php echo get_page_url("cdn", $CONF); ?>/<?php echo $CONF['template']; ?>/cache/<?php echo $CONF['page']; ?>.teknik.min.css" rel="stylesheet" />
     <script src="<?php echo get_page_url("cdn", $CONF); ?>/<?php echo $CONF['template']; ?>/cache/<?php echo $CONF['page']; ?>.teknik.min.js"></script>
   </body>
 </html>
