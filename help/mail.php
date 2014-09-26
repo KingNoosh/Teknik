742d7ab669e6ca100f57f436f6dbbc6b608a1076
Uncled1023 <admin@teknik.io>
2014-09-25 21:21:58 -0700

    Testing new mail format

--- help/mail.php
+++ help/mail.php
@@ -7,23 +7,35 @@
       You can either access your email via the <a href="<?php echo get_page_url("mail", $CONF); ?>" target="_blank">Web Client</a> or by using a client of your choosing with support for IMAP or POP3.
   </p>
   <div class="row">
-    <div class="col-sm-5">
-      <ul class="list-group">
-        <li class="list-group-item text-center"><h4>Outlook</h4></li>
-        <li class="list-group-item text-center">User Information</li>
-        <li class="list-group-item">Email Address:<div class="pull-right"><b>[username]@<?php echo $CONF['host']; ?></b></div></li>
-        <li class="list-group-item text-center">Server Information</li>
-        <li class="list-group-item">Incoming Server:<div class="pull-right"><b>mail.<?php echo $CONF['host']; ?></b></div></li>
-        <li class="list-group-item">Outgoing Server:<div class="pull-right"><b>mail.<?php echo $CONF['host']; ?></b></div></li>
-        <li class="list-group-item text-center">Logon Information</li>
-        <li class="list-group-item">Username:<div class="pull-right"><b>[username]@<?php echo $CONF['host']; ?></b></div></li>
-        <li class="list-group-item">Password:<div class="pull-right"><b>[password]</b></div></li>
-        <li class="list-group-item text-center">More Settings</li>
-        <li class="list-group-item">Requires Authentication:<div class="pull-right"><b>Both</b></div></li>
-        <li class="list-group-item">Incoming Server (IMAP):<div class="pull-right"><b>143 (993 SSL)</b></div></li>
-        <li class="list-group-item">Outgoing Server (SMTP):<div class="pull-right"><b>25 (465 SSL)</b></div></li>
-      </ul>
-    </div>
+    <ul class="list-group">
+      <li class="list-group-item text-center"><h4>Outlook</h4></li>
+      <li class="list-group-item">
+        <div class="col-sm-5">
+          <ul class="list-group">
+            <li class="list-group-item text-center">User Information</li>
+            <li class="list-group-item">Email Address:<div class="pull-right"><b>[username]@<?php echo $CONF['host']; ?></b></div></li>
+            <li class="list-group-item text-center">Server Information</li>
+            <li class="list-group-item">Incoming Server:<div class="pull-right"><b>mail.<?php echo $CONF['host']; ?></b></div></li>
+            <li class="list-group-item">Outgoing Server:<div class="pull-right"><b>mail.<?php echo $CONF['host']; ?></b></div></li>
+          </ul>
+        </div>
+      </li>
+      <li class="list-group-item">
+        <div class="col-sm-5">
+          <ul class="list-group">
+            <li class="list-group-item text-center">Logon Information</li>
+            <li class="list-group-item">Username:<div class="pull-right"><b>[username]@<?php echo $CONF['host']; ?></b></div></li>
+            <li class="list-group-item">Password:<div class="pull-right"><b>[password]</b></div></li>
+            <li class="list-group-item text-center">More Settings</li>
+            <li class="list-group-item">Requires Authentication:<div class="pull-right"><b>Both</b></div></li>
+            <li class="list-group-item">Incoming Server (IMAP):<div class="pull-right"><b>143 (993 SSL)</b></div></li>
+            <li class="list-group-item">Outgoing Server (SMTP):<div class="pull-right"><b>25 (465 SSL)</b></div></li>
+          </ul>
+        </div>
+      </li>
+    </ul>
+  </div>
+  <div class="row">
     <div class="col-sm-5 col-sm-offset-2">
       <ul class="list-group">
         <li class="list-group-item text-center"><h4>Thunderbird</h4></li>
