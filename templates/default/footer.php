9bc9dd87270838fd19b73cdb29f3938bca92a639
Uncled1023 <admin@teknik.io>
2014-09-25 19:16:36 -0700

    Moved javascript to footer

--- templates/default/footer.php
+++ templates/default/footer.php
@@ -4,5 +4,6 @@
           <p class="text-muted">&copy; Teknik 2013-2014 | <a href="<?php echo get_page_url("privacy", $CONF); ?>">Privacy</a> | <a href="<?php echo get_page_url("transparency", $CONF); ?>">Transparency</a> | <a href="<?php echo get_page_url("stats", $CONF); ?>">Stats</a></p>
       </div>
     </footer>
+    <script src="<?php echo get_page_url("cdn", $CONF); ?>/<?php echo $CONF['template']; ?>/cache/<?php echo $CONF['page']; ?>.teknik.min.js"></script>
   </body>
 </html>
