6c3fa50c75ca69d43307ffe273c84d66349c0020
Uncled1023 <admin@teknik.io>
2014-09-29 13:14:53 -0700

    Added stats to repo, and adding tracking to downloads

--- upload/lib/download.php
+++ upload/lib/download.php
@@ -1,5 +1,6 @@
 <?php
 include('../../includes/config.php');
+require_once('../../includes/PiwikTracker.php');
 $fileURL = $_GET['file'];
 $file_db = $db->select('uploads', "url=? LIMIT 1", array($fileURL));
 $file_path  = $CONF['upload_dir'] . $file_db['filename'];
@@ -31,12 +32,27 @@ if (file_exists($file_path) && $file_db)
   {
     header('Content-Type: '.$file_type);
     header('Content-Length: '.$file_db['filesize']);
-  }  
+  }
   readfile($file_path);
   if ($file_db['hash'] != "")
   {
     $result = unlink($file_path);
   }
+  $t = new PiwikTracker( $idSite = 1, get_page_url('stats', $CONF));
+
+  // Optional function calls
+  $t->setUserAgent($_SERVER['HTTP_USER_AGENT']);
+  $t->setUrlReferrer($_SERVER['HTTP_REFERER']);
+
+  // if you wanted to force to record the page view or conversion to a specific User ID
+  // $t->setUserId( "username@example.org" );
+  // Mandatory: set the URL being tracked
+  $t->setUrl( $url = get_page_url('u', $CONF).'/'.$fileURL );
+
+  // Finally, track the page view with a Custom Page Title
+  // In the standard JS API, the content of the <title> tag would be set as the page title
+  $t->doTrackPageView('Teknik Upload - '.$fileURL);
+
 }
 else
 {
