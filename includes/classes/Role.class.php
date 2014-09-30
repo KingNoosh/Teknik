574066208dd4bf4cef7d5afe956777f35ae424f6
Uncled1023 <admin@teknik.io>
2014-09-30 13:51:10 -0700

    Fixed getRole returning array

--- includes/classes/Role.class.php
+++ includes/classes/Role.class.php
@@ -37,18 +37,8 @@ class Role
     // Get Role by Name
     public function getRole($db, $role_name)
     {
-        $role_list = $db->select('roles', "role_name=?", array($role_name));
-        $roles = array();
-        foreach ($role_list as $role)
-        {
-          if (!is_array($role))
-          {
-            $roles = array($role_list);
-            break;
-          }
-          array_push($roles, $role);
-        }
-        return $roles;
+        $role = $db->select('roles', "role_name=? LIMIT 1", array($role_name));
+        return $role;
     }
     
     // Get all Roles
