23076e1c26dfd4dda809bfa0b5d2b5d600fc247d
Uncled1023 <admin@teknik.io>
2014-11-11 23:00:58 -0800

    Added additional git help

--- help/git.php
+++ help/git.php
@@ -28,6 +28,34 @@
     You can also view a list of the git repo's a user has by visiting: <code><?php echo get_page_url('git', $CONF); ?>/u/[username]/</code>
     <br />
   </p>
+  <h3>Cloning a user's git repository</h3>
+  <p>
+    To clone a users repo to a local copy, perform the following command: <code>~$git clone git://teknik.io/u/[username]/[reponame]</code>
+    <br />
+  </p>
+  <h3>Fork a user's git repository</h3>
+  <p>
+    When you fork a user's repository, you can make changes, and then send a pull request to the user to accept your changes.  To fork the user's repo you will need to perform the following commands.
+    <br />
+    <pre>
+      ~$git clone git://teknik.io/u/[username]/[repository]
+      ~$cd [repository]
+      ~$git remote set-url origin git@teknik.io:u/[your username]/[repository]
+      ~$git push
+    </pre>
+    <br />
+  </p>
+  <h3>Submit a pull request to the original author</h3>
+  <p>
+    In order to submit a pull request, you must have a forked repository of the original authors repository already in your remote repositories.
+    <br />
+    <br />
+    Once you have made your edits and pushed your commits, execute the following command: <code>~$git request-pull [original_commit] git://teknik.io/u/[your username]/[repository] [branch name]</code>
+    <br />
+    <br />
+    You will be given an output that will describe the pull request.  This must then be emailed to the original author to be considered for being pulled into the original repository.  The original author's email is <code>[username]@teknik.io</code>
+    <br />
+  </p>
   <h3>More Information</h3>
   <p>
     For more information on the commands available to you, just type: `~$ssh git@teknik.io help`
